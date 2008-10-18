90c0ea125668b8caba37cbf9a35abed5b6b5d9bc
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2008-10-18 12:48:34 +0000

    Initial commit for ocaml-gavl. Completely untested so far...
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406038

---
+++ src/gavl.ml
@@ -0,0 +1,166 @@
+(*
+ * Copyright 2003-2008 Savonet team
+ *
+ * This file is part of Ocaml-gavl.
+ *
+ * Ocaml-gavl is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocaml-gavl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ocaml-gavl; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *)
+
+module Video = 
+struct
+
+  exception Invalid_frame
+  exception Invalid_conversion
+
+  let _ = 
+    Callback.register_exception "caml_gavl_invalid_frame" Invalid_frame;
+    Callback.register_exception "caml_gavl_invalid_conversion" Invalid_conversion
+
+  type interlace_mode = 
+   | None         (* Progressive *) 
+   | Top_first    (* Top field first *)
+   | Bottom_first (* Bottom field first *)
+   | Mixed        (* Use interlace_mode of the frames *)
+
+  type pixel_format = 
+   | None         (* Undefined. *)
+   | Gray_8       (* 8 bit gray, scaled 0x00..0xff *)
+   | Gray_16      (* 16 bit gray, scaled 0x0000..0xffff *)
+   | Gray_float   (* floating point gray, scaled 0.0..1.0 *)
+   | Graya_16     (* 8 bit gray + alpha, scaled 0x00..0xff *)
+   | Graya_32     (* 16 bit gray + alpha, scaled 0x0000..0xffff *)
+   | Graya_float  (* floating point gray + alpha, scaled 0.0..1.0 *)
+   | Rgb_15       (* 15 bit RGB. Each pixel is a uint16_t in native byte order. 
+                   * Color masks are: for red: 0x7C00, for green: 0x03e0, for blue: 0x001f *)
+   | Bgr_15       (* 15 bit BGR. Each pixel is a uint16_t in native byte order. 
+                   * Color masks are: for red: 0x001f, for green: 0x03e0, for blue: 0x7C00 *)
+   | Rgb_16       (* 16 bit RGB. Each pixel is a uint16_t in native byte order. 
+                   * Color masks are: for red: 0xf800, for green: 0x07e0, for blue: 0x001f *)
+   | Bgr_16       (* 16 bit BGR. Each pixel is a uint16_t in native byte order. 
+                   * Color masks are: for red: 0x001f, for green: 0x07e0, for blue: 0xf800 *)
+   | Rgb_24       (* 24 bit RGB. Each color is an uint8_t. Color order is RGBRGB *)
+   | Bgr_24       (* 24 bit BGR. Each color is an uint8_t. Color order is BGRBGR *)
+   | Rgb_32       (* 32 bit RGB. Each color is an uint8_t. Color order is RGBXRGBX, where X is unused *)
+   | Bgr_32       (* 32 bit BGR. Each color is an uint8_t. Color order is BGRXBGRX, where X is unused *)
+   | Rgba_32      (* 32 bit RGBA. Each color is an uint8_t. Color order is RGBARGBA *)
+   | Rgb_48       (* 48 bit RGB. Each color is an uint16_t in native byte order. Color order is RGBRGB *)
+   | Rgba_64      (* 64 bit RGBA. Each color is an uint16_t in native byte order. Color order is RGBARGBA *)
+   | Rgb_float    (* float RGB. Each color is a float (0.0 .. 1.0) in native byte order. Color order is RGBRGB *)
+   | Rgba_float   (* float RGBA. Each color is a float (0.0 .. 1.0) in native byte order. Color order is RGBARGBA *)
+   | Yuy2         (* Packed YCbCr 4:2:2. Each component is an uint8_t. Component order is Y1 U1 Y2 V1 *)
+   | Yuvy         (* Packed YCbCr 4:2:2. Each component is an uint8_t. Component order is U1 Y1 V1 Y2 *)
+   | Yuva_32      (* Packed YCbCrA 4:4:4:4. Each component is an uint8_t. 
+                   * Component order is YUVA. Luma and chroma are video scaled, alpha is 0..255. *)
+   | Yuva_64      (* Packed YCbCrA 4:4:4:4. Each component is an uint16_t. 
+                   * Component order is YUVA. Luma and chroma are video scaled, alpha is 0..65535. *)
+   | Yuv_float    (* Packed YCbCr 4:4:4. Each component is a float. Luma is scaled 0.0..1.0, chroma is -0.5..0.5 *)
+   | Yuva_float   (* Packed YCbCrA 4:4:4:4. Each component is a float. Luma is scaled 0.0..1.0, chroma is -0.5..0.5 *)
+   | Yuv_420_p    (* Packed YCbCrA 4:4:4:4. Each component is an uint16_t. 
+                   * Component order is YUVA. Luma and chroma are video scaled, alpha is 0..65535. *)
+   | Yuv_422_p    (* Planar YCbCr 4:2:2. Each component is an uint8_t *)
+   | Yuv_444_p    (* Planar YCbCr 4:4:4. Each component is an uint8_t *)
+   | Yuv_411_p    (* Planar YCbCr 4:1:1. Each component is an uint8_t *)
+   | Yuv_410_p    (* Planar YCbCr 4:1:0. Each component is an uint8_t *)
+   | Yuvj_420_p   (* Planar YCbCr 4:2:0. Each component is an uint8_t, 
+                   * luma and chroma values are full range (0x00 .. 0xff) *)
+   | Yuvj_422_p   (* Planar YCbCr 4:2:2. Each component is an uint8_t, 
+                   * luma and chroma values are full range (0x00 .. 0xff) *)
+   | Yuvj_444_p   (* Planar YCbCr 4:4:4. Each component is an uint8_t, 
+                   * luma and chroma values are full range (0x00 .. 0xff) *)
+   | Yuv_444_p_16 (* 16 bit Planar YCbCr 4:4:4. Each component is an uint16_t in native byte order. *)
+   | Yuv_422_p_16 (* 16 bit Planar YCbCr 4:2:2. Each component is an uint16_t in native byte order. *)
+
+  type framerate_mode = 
+   | Constant  (* Constant framerate *)
+   | Variable  (* Variable framerate *)
+   | Still     (* Still image        *)
+
+  type chroma_placement = 
+   | Default (* MPEG-1/JPEG *)
+   | Mpeg2   (* MPEG-2      *)
+   | Dvpal   (* DV PAL      *)
+
+  type format = 
+  { 
+    frame_width      : int;
+    frame_height     : int;
+    image_width      : int;  
+    image_height     : int;
+    pixel_width      : int;
+    pixel_height     : int;
+    pixelformat      : pixel_format;
+    frame_duration   : int;
+    timescale        : int;
+    framerate_mode   : framerate_mode;
+    chroma_placement : chroma_placement;
+    interlace_mode   : interlace_mode
+  }
+
+  type internal_format =
+  {
+    _frame_width      : int;
+    _frame_height     : int;
+    _image_width      : int;
+    _image_height     : int;
+    _pixel_width      : int;
+    _pixel_height     : int;
+    _pixelformat      : int;
+    _frame_duration   : int;
+    _timescale        : int;
+    _framerate_mode   : framerate_mode;
+    _chroma_placement : chroma_placement;
+    _interlace_mode   : interlace_mode
+  }
+
+  external int_of_pf : pixel_format -> int = "caml_gavl_vid_format_of_index"
+
+  let internal_format_of_format f = 
+  {
+    _frame_width      = f.frame_width;
+    _frame_height     = f.frame_height;
+    _image_width      = f.image_width;
+    _image_height     = f.image_height;
+    _pixel_width      = f.pixel_width;
+    _pixel_height     = f.pixel_height;
+    _pixelformat      = int_of_pf f.pixelformat;
+    _frame_duration   = f.frame_duration;
+    _timescale        = f.timescale;
+    _framerate_mode   = f.framerate_mode;
+    _chroma_placement = f.chroma_placement;
+    _interlace_mode   = f.interlace_mode
+  }
+
+  type plane = Bigarray.int8_unsigned_elt
+
+  type frame = 
+  {
+    planes         : (plane*int) array;
+    timestamp      : Int64.t;
+    duration       : Int64.t; 
+    interlace_mode : interlace_mode
+  }
+
+  type converter
+
+  external create_converter : internal_format -> internal_format -> converter = "caml_gavl_vid_conv_create"
+
+  let create_converter f g = 
+    create_converter (internal_format_of_format f)
+                     (internal_format_of_format g)
+
+  external convert : converter -> frame -> frame = "caml_gavl_vid_conv_convert"
+
+end
+
---
+++ src/gavl_stubs.c
@@ -0,0 +1,252 @@
+/*
+  Copyright 2003-2008 Savonet team
+
+  This file is part of Ocaml-gavl.
+
+  Ocaml-gavl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Ocaml-gavl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Ocaml-gavl; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <caml/custom.h>
+#include <caml/fail.h>
+#include <caml/memory.h>
+#include <caml/signals.h>
+#include <caml/alloc.h>
+#include <caml/bigarray.h>
+#include <caml/callback.h>
+
+#include <string.h>
+#include <assert.h>
+#include <gavl/gavl.h>
+
+/* Video conversion */
+
+static gavl_video_format_t *video_format_of_value(value v, gavl_video_format_t *format)
+{
+  int i = 0;
+  format->frame_width      = Int_val(Field(v, i++));
+  format->frame_height     = Int_val(Field(v, i++));
+  format->image_width      = Int_val(Field(v, i++));
+  format->image_height     = Int_val(Field(v, i++));
+  format->pixel_width      = Int_val(Field(v, i++));
+  format->pixel_height     = Int_val(Field(v, i++));
+  format->pixelformat      = Int_val(Field(v, i++));
+  format->frame_duration   = Int_val(Field(v, i++));
+  format->timescale        = Int_val(Field(v, i++));
+  format->framerate_mode   = Int_val(Field(v, i++));
+  format->chroma_placement = Int_val(Field(v, i++));
+  format->interlace_mode   = Int_val(Field(v, i++));
+
+  return format;
+}
+
+/* Computes the size of a plane with index i 
+ * Taken from gavl's gavl_video_frame_copy_plane */
+static inline int caml_gavl_plane_size(gavl_video_format_t *format, int plane)
+{
+  int bytes_per_line;
+  int sub_h, sub_v;
+  int height = format->image_height;
+  sub_h = 1;
+  sub_v = 1;
+
+  bytes_per_line = gavl_pixelformat_is_planar(format->pixelformat) ?
+    format->image_width * gavl_pixelformat_bytes_per_component(format->pixelformat) :
+    format->image_width * gavl_pixelformat_bytes_per_pixel(format->pixelformat);
+
+  if(plane > 0)
+    {
+    gavl_pixelformat_chroma_sub(format->pixelformat, &sub_h, &sub_v);
+    bytes_per_line /= sub_h;
+    height /= sub_v;
+    }
+
+  return height * bytes_per_line; 
+}
+
+/* Frame here should have been initialized 
+ * with the given video format ! */
+static gavl_video_frame_t *gavl_video_frame_of_value(value v, gavl_video_format_t *vf, gavl_video_frame_t *f)
+{
+  int i = 0;
+  int j,len;
+  struct caml_ba_array *data;
+  value tmp,plane;
+  gavl_pixelformat_t pf = vf->pixelformat;
+  int np = gavl_pixelformat_num_planes(pf);
+  if (np != Wosize_val(v))
+    caml_raise_constant(*caml_named_value("caml_gavl_invalid_frame"));
+  value planes = Field(v, i++);
+  if (Wosize_val(planes) > GAVL_MAX_PLANES)
+    caml_failwith("more than GAVL_MAX_PLANES planes");
+
+  for (j = 0; j < np; j++)
+  {
+    tmp = Field(planes,j);
+    plane = Field(tmp,0);
+    data = Caml_ba_array_val(plane);
+    len = caml_gavl_plane_size(vf,j);
+    if (data->num_dims != 1 || data->dim[0] != len)
+      caml_raise_constant(*caml_named_value("caml_gavl_invalid_frame"));
+    memcpy(f->planes[j],data,len);
+    f->strides[j] = Int_val(Field(tmp,1));
+  }
+
+  f->timestamp      = Int64_val(Field(v,i++));
+  f->duration       = Int64_val(Field(v,i++));
+  f->interlace_mode = Int_val(Field(v,i++));
+
+  return f;
+}
+
+/* Warning: this assumes that the frame has been initialized for the corresponding
+ * video format ! */
+static value value_of_gavl_video_frame(gavl_video_format_t *vf, gavl_video_frame_t *f)
+{
+  CAMLparam0();
+  CAMLlocal4(v,planes,tmp,p);
+  v = caml_alloc_tuple(4);
+  int i = 0;
+  int j;
+  intnat len;
+  void *data;
+  gavl_pixelformat_t pf = vf->pixelformat;
+  int np = gavl_pixelformat_num_planes(pf);
+  planes = caml_alloc_tuple(np);
+  for (j = 0; j < np; j++)
+  {
+    if (f->planes[j] == NULL)
+      caml_raise_constant(*caml_named_value("caml_gavl_invalid_frame"));
+    tmp = caml_alloc_tuple(2);
+    len = caml_gavl_plane_size(vf,j);
+    data = malloc(len);
+    if (data == NULL)
+      caml_failwith("malloc");
+    p = caml_ba_alloc(CAML_BA_MANAGED|CAML_BA_C_LAYOUT|CAML_BA_UINT8,1,data,&len);
+    memcpy(data,f->planes[j],len);
+    Store_field (tmp,0,p);
+    Store_field (tmp,1,Val_int(f->strides[j]));
+    Store_field (planes,j,tmp);
+  }
+  Store_field (v, i++, planes);
+  Store_field (v, i++, caml_copy_int64(f->timestamp));
+  Store_field (v, i++, caml_copy_int64(f->duration));
+  Store_field (v, i++, Int_val(f->interlace_mode));
+
+  CAMLreturn(v);
+}
+
+typedef struct video_converter_t
+{
+  /* Converter object */
+  gavl_video_converter_t *conv;
+  /* Number of needed passes, as returned by init */
+  int pass;
+  gavl_video_format_t in_vf;
+  gavl_video_format_t out_vf;
+} vid_conv_t;
+
+#define Vid_conv_val(v) (*((vid_conv_t**)Data_custom_val(v)))
+
+static void finalize_vid_conv(value v)
+{
+  vid_conv_t *conv = Vid_conv_val(v);
+  gavl_video_converter_destroy(conv->conv);
+  free(conv);
+}
+
+static struct custom_operations vid_conv_ops =
+{
+  "ocaml_gavl_vid_conv",
+  finalize_vid_conv,
+  custom_compare_default,
+  custom_hash_default,
+  custom_serialize_default,
+  custom_deserialize_default
+};
+
+CAMLprim value caml_gavl_vid_format_of_index(value i)
+{
+  CAMLparam0();
+  CAMLreturn(Val_int(gavl_get_pixelformat(Int_val(i))));
+}
+
+CAMLprim value caml_gavl_vid_conv_create(value old, value new)
+{
+  CAMLparam0();
+  CAMLlocal1(ret);
+  int pass;
+  vid_conv_t *conv = malloc(sizeof(vid_conv_t));
+  if (conv == NULL) 
+    caml_failwith("malloc");
+
+  conv->conv = gavl_video_converter_create();
+  if (conv->conv == NULL)
+    caml_failwith("gavl_video_converter_create");
+  video_format_of_value(old,&conv->in_vf);
+  video_format_of_value(new,&conv->out_vf);
+  pass = gavl_video_converter_init(conv->conv,&conv->in_vf,&conv->out_vf);
+  
+  if (pass == -1)
+  {
+    free(conv);
+    caml_raise_constant(*caml_named_value("caml_gavl_invalid_conversion"));
+  }
+
+  conv->pass  = pass;
+
+  ret = caml_alloc_custom(&vid_conv_ops, sizeof(vid_conv_t *), 1, 0);
+  Vid_conv_val(ret) = conv;
+
+  CAMLreturn(ret);
+}
+
+CAMLprim value caml_gavl_vid_conv_convert(value conv, value old) 
+{
+  CAMLparam2(conv,old);
+  CAMLlocal1(ret);
+  vid_conv_t *vid_conv = Vid_conv_val(conv);
+
+  /* pass < 0 should not happen since it 
+     cannot be instanciated like that..*/
+  assert(vid_conv->pass >= 0);
+  if (vid_conv->pass == 0)
+    CAMLreturn(old);
+
+  gavl_video_converter_t *cnv = vid_conv->conv;
+  gavl_video_frame_t *inf = gavl_video_frame_create(&vid_conv->in_vf);
+  gavl_video_frame_t *outf = gavl_video_frame_create(&vid_conv->out_vf);
+  if (inf == NULL || outf == NULL)
+    caml_failwith("malloc");
+  int j;
+
+  gavl_video_frame_of_value(old,&vid_conv->in_vf,inf);
+
+  /* pass == 0 means no conversion is needed.. 
+   * frame is copied.. */
+  if (vid_conv->pass == 0)
+    CAMLreturn(value_of_gavl_video_frame(&vid_conv->in_vf,inf));
+
+  caml_enter_blocking_section();
+  for (j = 0; j < vid_conv->pass; j++)
+    gavl_video_convert(cnv,inf,outf);
+  caml_leave_blocking_section();  
+
+  ret = value_of_gavl_video_frame(&vid_conv->out_vf,outf);
+  
+  gavl_video_frame_destroy(inf);
+  gavl_video_frame_destroy(outf);
+
+  CAMLreturn(ret);
+}
